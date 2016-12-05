d3e10dd6acb863ca4439c55e4d9aab072f2557c7
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-12-05 15:11:44 +0530

    Create PosorNeg.java

---
+++ PosorNeg.java
@@ -0,0 +1,15 @@
+class PosorNeg
+{
+public static void main (String[] args)
+{
+Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+if(num>=0)
+{
+System.out.println("positive");
+}
+else
+{
+system.out.println("negative");
+}
+}
