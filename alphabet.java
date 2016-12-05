f815919ee982335ffa2417a6cbe9a1f23239c1b0
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-12-05 15:32:22 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,13 @@
+class consorstring
+{
+public static void main(String[] args)
+{
+
+for(char i=='A';i<='Z';i++)
+{
+System.out.println("alphabet uppercase");
+}
+for(char i=='a';i<='z';i++) {
+System.out.println("alphabet lowercase");
+}
+}
