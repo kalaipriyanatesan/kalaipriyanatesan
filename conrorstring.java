6139eff18771865550f12ab5555a99781e96fce4
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-12-05 15:25:50 +0530

    Create conrorstring.java

---
+++ conrorstring.java
@@ -0,0 +1,12 @@
+class consorstring
+{
+public static void main(String[] args)
+{
+if(char=='A'|| char=='E' ||char=='I' || char=='O' || char=='U'||char=='a'|| char=='e' ||char=='i' || char=='o' || char=='u')
+{System.out.println("vovwel");
+}
+else
+{
+System.out.println("consonent");
+}
+}
